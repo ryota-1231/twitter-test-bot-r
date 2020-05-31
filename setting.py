4da5e1b4b96dd7e0eccca91b158bc9090cba8a58
ryota-1231 <rrrrr.1231@gmail.com>
2020-05-31 17:59:26 +0900

    gitignore create

---
+++ setting.py
@@ -0,0 +1,13 @@
+# coding: UTF-8
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# 環境変数の値をAPに代入
+API_KEY= os.environ.get("API_KEY") 
+API_SECRET_KEY = os.environ.get("API_SECRET_KEY") 
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN") 
+ACCESS_TOKEN_SECRET =os.environ.get("ACCESS_TOKEN_SECRET") 
